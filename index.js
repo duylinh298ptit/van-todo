67d9023afd686d7e0cfc0a14fd9dda0c5918a4c6
Nguyen Duy Linh <duylinh196tb@gmail.com>
2018-11-08 01:48:04 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,133 @@
+import express from "express";
+import bodyParser from "body-parser";
+// export để chạy unit test thôi
+export const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+export let listTodos = [
+  {
+    id: 1,
+    todo: "Hoc bai cu",
+    done: true
+  },
+  {
+    id: 2,
+    todo: "Uong nuoc",
+    done: false
+  },
+  {
+    id: 3,
+    todo: "Hoc code",
+    done: false
+  }
+];
+
+// Lay tat ca danh sach todo
+app.get("/todos", (req, res) => {
+  res.json({
+    data: listTodos,
+    message: "Lay danh sach todo thanh cong"
+  });
+});
+
+// Lay todo theo id
+app.get("/todos/:id", (req, res) => {
+  // B1: Lay param id
+  const id = req.params.id;
+  // Tìm index của todo theo id( sử dụng findIndex), nếu có todo thì index = todo index, nếu không có thì index = -1
+  const index = listTodos.findIndex(todo => {
+    return todo.id == id;
+  });
+  // Neu index = -1 => todo theo id khong ton tai, tra ve loi
+  if (index === -1) {
+    res.status(404);
+    res.json({
+      message: "Todo khong ton tai"
+    });
+  }
+
+  // Neu index !== -1 => todo = listTodos[index]
+  res.json({
+    data: listTodos[index],
+    message: "Lay todo thanh cong"
+  });
+});
+
+// Tạo một todo
+app.post("/todos", (req, res) => {
+  const todo = req.body.todo;
+  // tao mot todo
+  const newTodo = {
+    id: Math.random(), // tao 1 id ngau nhien
+    todo: todo,
+    done: false // false = chua lam xong
+  };
+
+  listTodos.push(newTodo);
+
+  res.json({
+    data: newTodo,
+    message: "Tạo mới todo thành công"
+  });
+});
+
+// Xoa 1 todo
+app.delete("/todos/:id", (req, res) => {
+  // B1: Lay param id
+
+  const id = req.params.id;
+  // Tìm index của todo theo id( sử dụng findIndex), nếu có todo thì index = todo index, nếu không có thì index = -1
+  const index = listTodos.findIndex(todo => {
+    return todo.id == id;
+  });
+  // Neu index = -1 => todo theo id khong ton tai, tra ve loi
+  if (index === -1) {
+    res.status(404);
+    res.json({
+      message: "Todo khong ton tai"
+    });
+  }
+
+  // Neu index !== -1 => dung splice de xoa todo
+  listTodos.splice(index, 1);
+  res.json({
+    data: id,
+    message: "Xoa todo thanh cong"
+  });
+});
+
+// Update 1 todo
+app.patch("/todos/:id", (req, res) => {
+  // B1: Lay param id
+  const id = req.params.id;
+  // Tìm index của todo theo id( sử dụng findIndex), nếu có todo thì index = todo index, nếu không có thì index = -1
+  const index = listTodos.findIndex(todo => {
+    return todo.id == id;
+  });
+  // Neu index = -1 => todo theo id khong ton tai, tra ve loi
+  if (index === -1) {
+    res.status(404);
+    res.json({
+      message: "Todo khong ton tai"
+    });
+  }
+
+  // Tao moi 1 todo
+  const newTodo = {
+    ...listTodos[index],
+    ...req.body
+  };
+  // Neu index !== -1 => dung splice de  cap nhat
+  listTodos.splice(index, 1, newTodo);
+  res.json({
+    data: newTodo,
+    message: "Cap nhat todo thanh cong"
+  });
+});
+
+// khong can quan tam process env NODE_ENV
+if (process.env.NODE_ENV !== "test") {
+  app.listen(3333, () => {
+    console.log("> Server running");
+  });
+}
---
+++ testapi.spec.js
@@ -0,0 +1,95 @@
+import request from "supertest";
+import { app, listTodos } from "./index";
+
+describe("/GET", () => {
+  test("Lay tat ca danh sach Todo", done => {
+    request(app)
+      .get("/todos")
+      .expect(200)
+      .end((err, res) => {
+        expect(res.body.data).toEqual(listTodos);
+        done();
+      });
+  });
+
+  test("Lay todo theo id", done => {
+    request(app)
+      .get("/todos/" + listTodos[0].id)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.body.data).toEqual(listTodos[0]);
+        done();
+      });
+  });
+
+  test("Tra ve loi neu todo khong ton tai", done => {
+    request(app)
+      .get("/todos/" + listTodos[0].id + "x")
+      .expect(404)
+      .end((err, res) => {
+        done();
+      });
+  });
+});
+
+describe("/POST", () => {
+  test("Tao moi todo", done => {
+    request(app)
+      .post("/todos")
+      .send({ todo: "xxx" })
+      .expect(200)
+      .end((err, res) => {
+        expect(res.body.data.todo).toBe("xxx");
+        expect(listTodos[listTodos.length - 1].todo).toBe("xxx");
+        done();
+      });
+  });
+});
+describe("/DELETE", () => {
+  test("Xoa todo theo id", done => {
+    const fakeId = listTodos[0].id;
+    request(app)
+      .delete("/todos/" + fakeId)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.body.data == fakeId).toEqual(true);
+        listTodos.forEach(todo => expect(todo.id == fakeId).toEqual(false));
+        done();
+      });
+  });
+
+  test("Tra ve loi neu todo khong ton tai", done => {
+    request(app)
+      .delete("/todos/" + listTodos[0].id + "x")
+      .expect(404)
+      .end((err, res) => {
+        done();
+      });
+  });
+});
+describe("/PATCH", () => {
+  test("Update todo theo id", done => {
+    const fakeData = {
+      id: listTodos[0].id,
+      todo: "xxx",
+      done: true
+    };
+    request(app)
+      .patch("/todos/" + fakeData.id)
+      .send(fakeData)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.body.data).toEqual(fakeData);
+        done();
+      });
+  });
+
+  test("Tra ve loi neu todo khong ton tai", done => {
+    request(app)
+      .patch("/todos/" + listTodos[0].id + "x")
+      .expect(404)
+      .end((err, res) => {
+        done();
+      });
+  });
+});
